dd8f6018e342c1d7104cf182a4d6a4a44d2bb6ee
RagingQuiche <lar25021@byui.edu>
2026-09-18 21:20:47 -0600

    Completed first two syntax projects.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is the grade percentage? ");
+        string input = Console.ReadLine();
+        float gradePercentage = float.Parse(input);
+
+        string letterGrade;
+        bool passFail = true;
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }    
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+            passFail = false;
+        }
+        else
+        {
+            letterGrade = "F";
+            passFail = false;
+        }
+
+
+        if (passFail == true)
+        {
+            Console.WriteLine($"You passed! Your letter grade is {letterGrade}.");
+        }
+        else
+        {
+            Console.WriteLine($"Your letter grade is {letterGrade}. Hopefully you can do better next time.");
+        }
+        }   
 }
