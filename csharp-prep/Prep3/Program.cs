cb00ea85f798a3d24684247c088903675c92e115
RagingQuiche <lar25021@byui.edu>
2026-09-23 17:23:38 -0600

    Completed Guess My Number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        Console.Write("What is the magic numer? ");
+
+        int correct = 0;
+        do
+        {
+            string input = Console.ReadLine();
+            int guess = int.Parse(input);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What's your new guess? ");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What's your new guess? ");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (correct != 1);
     }
 }
